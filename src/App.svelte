e2c6d7e27bd44a27ea58826a4d1d2ce4111ede81
Jonas Holbech <jh@jonasholbech.dk>
2022-04-08 11:49:03 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Form from "./Form.svelte";
+  import TodoItem from "./TodoItem.svelte";
+
+  let tasks = [
+    {
+      id: Math.random(),
+      task: "Clean House",
+      completed: false,
+      added: Date.now(),
+    },
+  ];
+
+  function addTask(newTask) {
+    tasks = tasks.concat(newTask);
+  }
+  function deleteTask(id) {
+    console.log(id);
+    tasks = tasks.filter((item) => item.id !== id);
+  }
+  function toggleTask(id) {
+    tasks = tasks.map((item) => {
+      if (item.id === id) {
+        item.completed = !item.completed;
+      }
+      return item;
+    });
+  }
+</script>
+
+<Form {addTask} />
+<section>
+  <ol>
+    {#each tasks as task}
+      <TodoItem {...task} {deleteTask} {toggleTask} />
+    {/each}
+  </ol>
+</section>
---
+++ src/Form.svelte
@@ -0,0 +1,19 @@
+<script>
+  let task;
+  export let addTask;
+  function submit(e) {
+    addTask({
+      task: task,
+      id: Math.random(),
+      completed: false,
+      added: Date.now(),
+    });
+    task = "";
+  }
+</script>
+
+<form on:submit|preventDefault={submit}>
+  <label for="task">Task</label>
+  <input type="text" bind:value={task} id="task" name="task" />
+  <button>Add Task</button>
+</form>
